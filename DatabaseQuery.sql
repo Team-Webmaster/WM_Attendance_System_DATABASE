efe532eaa96ba05913bbc98bf278f7fb18c6ac6f
LakSK98 <lakshitha199809@gmail.com>
2022-06-30 00:40:15 +0530

    Query updated

--- DatabaseQuery.sql
+++ DatabaseQuery.sql
@@ -16,7 +16,7 @@ CREATE TABLE userTable(
 	telephone varchar(13),
 	profile_pic varchar(200),
 	type int,
-	no_of_annual_leaves int,
+	no_of_annual_leaves real,
 	constraint PK_user primary key(user_id)
 );
 
@@ -32,7 +32,7 @@ CREATE TABLE pendingUserTable(
 	type int,
 	no_of_annual_leaves int,
 	status varchar(15),
-        confirm int,
+	confirm int,
 	constraint PK_pending_user primary key(pending_user_id)
 );
 
@@ -44,17 +44,26 @@ CREATE TABLE blackListedEmails(
 CREATE TABLE shortLeaveDetails(
 	id int identity(1,1),
 	date Date,
-	start_time Time,
-	end_time Time,
+	start_time varchar(10),
+	end_time varchar(10),
 	special_notes varchar(100),
 	requester_id int,
 	approval_id int,
 	status varchar(15),
 	constraint PK_shortLeaveDetails primary key(id),
-	constraint FK1_shortLeaveDetails foreign key requester_id reference userTable(user_id),
-	constraint FK2_shortLeaveDetails foreign key approval_id reference userTable(user_id)
-)
-ALTER TABLE shortLeaveDetails NOCHECK CONSTRAINT FK2_shortLeaveDetails;
+	constraint FK1_shortLeaveDetails foreign key(requester_id) references userTable(user_id)
+);
+
+CREATE TABLE emergencyLeaveDetails(
+	id int identity(1,1),
+	reason varchar(300),
+	requester_id int,
+	approval_id int,
+	status varchar(15),
+	constraint PK_emergencyLeaveDetails primary key(id),
+	constraint FK1_emergencyLeaveDetails foreign key(requester_id) references userTable(user_id)
+);
+
 
 CREATE TABLE Attendance(
 	id int identity(1,1),
@@ -68,17 +77,18 @@ CREATE TABLE Attendance(
 );
 
 CREATE TABLE VideoConference(
-	conference_id int identity(1,1),
-	date Date,
-	time Time,
-	host_id varchar(10),
+	conference_id varchar(20),
+	date varchar(15),
+	time varchar(10),
+	host_id int,
 	scheduler_id int,
 	constraint PK_VideoConference primary key(conference_id),
-	constraint FK_VideoConference foreign key(scheduler_id) references userTable(user_id)
+	constraint FK1_VideoConference foreign key(scheduler_id) references userTable(user_id),
+	constraint FK2_VideoConference foreign key(host_id) references userTable(user_id)
 );
 
 CREATE TABLE VideoConferenceHasUser(
-	c_id int,
+	c_id varchar(20),
 	u_id int,
 	constraint PK_VCHU primary key(c_id,u_id),
 	constraint FK1_VideoConferenceHasUser foreign key(u_id) references userTable(user_id),
@@ -90,14 +100,17 @@ CREATE TABLE Report(
 	start_date Date,
 	end_date Date,
 	u_id int,
+	type varchar(30),
+	requester_id int,
 	constraint PK_Report primary key(report_id),
-	constraint FK_Report foreign key(u_id) references userTable(user_id)
+	constraint FK1_Report foreign key(u_id) references userTable(user_id),
+	constraint FK2_Report foreign key(requester_id) references userTable(user_id)
 );
 
 CREATE TABLE Settings(
 	settings_id int identity(1,1),
 	no_of_working_hours_per_day int,
-	no_of_annual_leaves int,
+	no_of_annual_leaves real,
 	no_of_working_days_per_week int,
 	admin_id int,
 	constraint PK_Settings primary key(settings_id),
@@ -117,22 +130,42 @@ CREATE TABLE Notification(
 );
 
 CREATE TABLE HolidayCalendarEvent(
-	id int identity(1,1),
+	id char(15),
 	name varchar(20),
 	date Date,
-	time Time,
+	duration real,
 	comment varchar(50),
 	editor_id int,
 	constraint PK_CalendarEvent primary key(id),
 	constraint FK_CalendarEvent foreign key(editor_id) references userTable(user_id)
 );
 
+CREATE TABLE ShortHolidayCalendarEvent(
+	id char(15),
+	name varchar(20),
+	date Date,
+	start_time varchar(6),
+	end_time varchar(6),
+	comment varchar(50),
+	editor_id int,
+	constraint PK_ShortHolidayCalendarEvent primary key(id),
+	constraint FK_ShortHolidayCalendarEvent foreign key(editor_id) references userTable(user_id)
+);
+
 CREATE TABLE HolidayCalendarEventHasUser(
-	calendar_event_id int,
+	calendar_event_id char(15),
 	u_id int,
 	constraint PK_HolidayCalendarEventHasUser primary key(calendar_event_id,u_id),
 	constraint FK1_HolidayCalendarEventHasUser foreign key(calendar_event_id) references HolidayCalendarEvent(id),
 	constraint FK2_HolidayCalendarEventHasUser foreign key(u_id) references userTable(user_id)
+);
+
+CREATE TABLE ShortHolidayCalendarEventHasUser(
+	calendar_event_id char(15),
+	u_id int,
+	constraint PK_ShortHolidayCalendarEventHasUser primary key(calendar_event_id,u_id),
+	constraint FK1_ShortHolidayCalendarEventHasUser foreign key(calendar_event_id) references ShortHolidayCalendarEvent(id),
+	constraint FK2_ShortHolidayCalendarEventHasUser foreign key(u_id) references userTable(user_id)
 );
 
 CREATE TABLE SMS(
@@ -184,7 +217,6 @@ CREATE TABLE LeaveDetails(
 	leave_type_id int,
 	approval_id int,
 	constraint PK_LeaveDetails primary key(leave_id),
-	constraint FK1_LeaveDetails foreign key(approval_id) references userTable(user_id),
 	constraint FK2_LeaveDetails foreign key(leave_type_id) references Leave(id),
 	constraint FK3_LeaveDetails foreign key(sender_id) references userTable(user_id)
 );
@@ -204,57 +236,86 @@ CREATE TABLE LeaveCalendarEvent(
 
 CREATE VIEW LeaveCalendarEvents
 AS
-SELECT u.user_id,u.name,lce.date,lce.time,lce.comment
-FROM LeaveCalendarEvent lce,userTable u
-WHERE lce.user_id=u.user_id
+SELECT u.user_id,ld.type,ld.date,ld.duration,ld.special_note,l.type leaveType
+FROM LeaveDetails ld,userTable u,Leave l
+WHERE ld.sender_id=u.user_id and ld.leave_type_id=l.id
+
+CREATE VIEW ShortLeaveCalendarEvents
+AS
+SELECT u.user_id,sld.date,sld.start_time,sld.end_time,sld.special_notes
+FROM ShortLeaveDetails sld,userTable u
+WHERE sld.requester_id=u.user_id and sld.status='Approved'
 
 CREATE VIEW HolidayCalendarEvents
 AS
-SELECT u.user_id,u.name,hce.date,hce.time,hce.comment
+SELECT u.user_id,hce.name,hce.date,hce.duration,hce.comment
 FROM HolidayCalendarEvent hce,userTable u,HolidayCalendarEventHasUser hcehu
 WHERE hcehu.u_id=u.user_id and hcehu.calendar_event_id=hce.id
 
+CREATE VIEW ShortHolidayCalendarEvents
+AS
+SELECT u.user_id,shce.name,shce.date,shce.start_time,shce.end_time,shce.comment
+FROM ShortHolidayCalendarEvent shce,userTable u,ShortHolidayCalendarEventHasUser shcehu
+WHERE shcehu.u_id=u.user_id and shcehu.calendar_event_id=shce.id
+
 CREATE VIEW PendingRequests
 AS
-SELECT u.name,r.date,r.time,u.nic,l.type
+SELECT r.request_id,u.name,r.date,u.nic,u.profile_pic,l.type,r.type duration_type,r.special_note,r.duration
 FROM Request r,userTable u,Leave l
 WHERE r.leave_type_id=l.id and r.sender_id=u.user_id and r.status='Pending'
 
-CREATE FUNCTION CalendarEventsByUser(@user_id char(10))
+CREATE VIEW PendingShortLeaveRequests
+AS
+SELECT s.id,u.name,u.nic,u.profile_pic,s.date,s.start_time,s.end_time,s.special_notes
+FROM shortLeaveDetails s, userTable u
+WHERE s.requester_id=u.user_id and s.status='Pending'
+
+CREATE VIEW pendingEmergencyLeaves
+AS
+SELECT u.name,u.nic,u.profile_pic,eld.reason,eld.id,u.user_id
+FROM emergencyLeaveDetails eld, userTable u
+WHERE eld.requester_id=u.user_id and eld.status='Pending'
+
+CREATE FUNCTION CalendarEventsByUser(@user_id int)
 RETURNS @myTable TABLE(
-	userId int,
-	userName varchar(30),
+	eventName varchar(75),
 	date Date,
-	time Time,
+	duration real,
+	comment varchar(50),
+	type varchar(20)
+)
+AS
+BEGIN
+	INSERT INTO @myTable
+	SELECT lce.leaveType,lce.date,lce.duration,lce.special_note,lce.type
+	FROM LeaveCalendarEvents lce
+	WHERE lce.user_id=@user_id
+	INSERT INTO @myTable
+	SELECT hce.name,hce.date,hce.duration,hce.comment,'Holiday'
+	FROM HolidayCalendarEvents hce
+	WHERE hce.user_id=@user_id
+	RETURN
+END
+
+CREATE FUNCTION ShortCalendarEventsByUser(@user_id int)
+RETURNS @myTable TABLE(
+	eventName varchar(75),
+	date Date,
+	startTime varchar(6),
+	endTime varchar(6),
 	comment varchar(50)
 )
 AS
 BEGIN
 	INSERT INTO @myTable
-	SELECT lce.user_id,lce.name,lce.date,lce.time,lce.comment
-	FROM LeaveCalendarEvents lce
-	WHERE lce.user_id=@user_id
-
+	SELECT 'Short Leave',slce.date,slce.start_time,slce.end_time,slce.special_notes
+	FROM ShortLeaveCalendarEvents slce
+	WHERE slce.user_id=@user_id
 	INSERT INTO @myTable
-	SELECT hce.user_id,hce.name,hce.date,hce.time,hce.comment
-	FROM HolidayCalendarEvents hce
-	WHERE hce.user_id=@user_id
-
+	SELECT shce.name,shce.date,shce.start_time,shce.end_time,shce.comment
+	FROM ShortHolidayCalendarEvents shce
+	WHERE shce.user_id=@user_id
 	RETURN
-END
-
-CREATE TRIGGER AddLeave
-ON Request
-AFTER UPDATE
-AS
-BEGIN
-	DECLARE @date Date
-	DECLARE @time Time
-	DECLARE @approval_id char(10)
-	DECLARE @leave_type_id char(10)
-	SELECT @date=date,@time=time,@approval_id=approval_id,@leave_type_id=leave_type_id
-	FROM inserted
-	INSERT INTO LeaveDetails VALUES(@date,@time,@approval_id,@leave_type_id)
 END
 
 CREATE FUNCTION AttendHours(@userId int,@start_date date,@end_date date)
@@ -336,4 +397,33 @@ BEGIN
 		INSERT INTO blackListedEmails VALUES(@email)
 		DELETE FROM pendingUserTable WHERE email=@email
 	END
+END
+SELECT * FROM Request
+SELECT * FROM userTable
+UPDATE userTable SET no_of_annual_leaves=20 WHERE user_id=1
+UPDATE Request SET status='Approved' WHERE request_id=4
+SELECT * FROM LeaveDetails
+DROP TRIGGER ApproveLeave
+CREATE TRIGGER ApproveLeave
+ON Request
+INSTEAD OF UPDATE
+AS
+BEGIN
+	DECLARE @id int
+	DECLARE @status varchar(15)
+	SELECT @id=request_id,@status=status FROM inserted
+	IF @status='Approved'
+	BEGIN
+		DECLARE @date DATE
+		DECLARE @type varchar(15)
+		DECLARE @duration real
+		DECLARE @special_note varchar(100)
+		DECLARE @sender_id int
+		DECLARE @leave_type_id int
+		DECLARE @approval_id int
+		SELECT @date=date,@type=type,@duration=duration,@special_note=special_note,@sender_id=sender_id,@leave_type_id=leave_type_id,@approval_id=approval_id FROM inserted
+		INSERT INTO LeaveDetails VALUES(@date,@type,@duration,@special_note,@sender_id,@leave_type_id,@approval_id)
+		UPDATE userTable SET no_of_annual_leaves=(no_of_annual_leaves-@duration) WHERE user_id=@sender_id
+	END
+	DELETE FROM Request WHERE request_id=@id
 END
